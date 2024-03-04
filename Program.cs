6fbd8d56cfc0657be94f713e1046baac67d6e955
Woedroe <118165188+WoutLar@users.noreply.github.com>
2024-03-04 15:08:04 +0100

    0.1 commit
    version 0.1 nothing added

--- Program.cs
+++ Program.cs
@@ -3,26 +3,12 @@
     class Program
     {
         public static string userInput;
-        public static string[] cardSet;
+        public static List<String>[] cardSet;
         public static bool debug;
+
         static void Main(string[] args)
         {
-            cardSet = CardDeck.CreateCardSet();
-
-            if (userInput == "debugOnn")
-            {
-                debug = true;
-                
-            }
             
-            if (debug)
-            {
-                foreach (string card in cardSet)
-                {
-                    Console.WriteLine(card);
-                }
-                BlackjackGame.StartGame();
-            }
         }
     }
 }
